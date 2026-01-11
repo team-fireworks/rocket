37d4c5a23a33f57ebb7c9e39d64297f1a104a1ee
znotfireman <znotfireman@gmail.com>
2026-01-12 00:21:23 +0700

    feat: insert cmd & docs start

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -14,12 +14,23 @@ export default defineConfig({
 					href: "https://github.com/team-fireworks/rocket",
 				},
 			],
+			customCss: [
+				"./src/assets/rocket.css",
+				"@fontsource-variable/inconsolata",
+				"@fontsource/source-sans-pro/200.css",
+				"@fontsource/source-sans-pro/300.css",
+				"@fontsource/source-sans-pro/400.css",
+				"@fontsource/source-sans-pro/600.css",
+				"@fontsource/source-sans-pro/700.css",
+				"@fontsource/source-sans-pro/900.css",
+			],
 			sidebar: [
 				{
 					label: "Guides",
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: "Example Guide", slug: "guides/example" },
+						{ label: "Temp Guide", slug: "temp" },
 					],
 				},
 				{
